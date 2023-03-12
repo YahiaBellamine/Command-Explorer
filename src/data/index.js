820f31d99752112e1d0d4921125e6d81eda505c0
Yahia Bellamine <92577857+YahiaBellamine@users.noreply.github.com>
2023-03-12 21:57:49 +0100

    Remove old data

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,7 @@
-export { primaryOptions as gitOptionsFirst } from './git-primary-options';
-export { secondaryOptions as gitOptionsSecond } from './git-secondary-options';
-export { tertiaryOptions as gitOptionsThird } from './git-tertiary-options';
+export { primaryOptions as gitOptionsFirst } from './git/primary-options';
+export { secondaryOptions as gitOptionsSecond } from './git/secondary-options';
+export { tertiaryOptions as gitOptionsThird } from './git/tertiary-options';
 
-export { primaryOptions as dockerOptionsFirst } from './docker-primary-options';
-export { secondaryOptions as dockerOptionsSecond } from './docker-secondary-options';
-export { tertiaryOptions as dockerOptionsThird } from './docker-tertiary-options';
+export { primaryOptions as dockerOptionsFirst } from './docker/primary-options';
+export { secondaryOptions as dockerOptionsSecond } from './docker/secondary-options';
+export { tertiaryOptions as dockerOptionsThird } from './docker/tertiary-options';
